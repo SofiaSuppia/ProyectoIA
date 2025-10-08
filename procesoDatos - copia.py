528c56b25797fe28384045acc7e31d6f7b06400d
CarlaGF19 <usernameexperience@gmail.com>
2025-10-07 20:52:41 -0500

    Verificacion
    Nuevo cambio

--- procesoDatos - copia.py
+++ procesoDatos - copia.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import calendar
-
+#comentario de prueba
 # --- CONFIGURACIÓN DE ARCHIVOS Y PARÁMETROS ---
 ARCHIVOS = {
     'clientes': 'Clientes.xlsx',
